d0b0e74cdec1d57878522949595b2c428496db11
Rishabh soni <80063042+RishabhSoni01@users.noreply.github.com>
2021-09-24 17:30:25 +0000

    Pointer Assignment and Dynamic_allocation

---
+++ DYNAMIC_ALLOCATION/default_arguments.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int getValue(int x = 0, int y = 0, int z){  
+    return (x + y + z); 
+}
+
+int main(){
+   cout << getValue(10);
+   return 0;
+}
+//output>>Compilation error due to Default parameters must be added last in C++ functions.
